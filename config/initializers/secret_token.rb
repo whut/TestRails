fd895056ac53318048a3d8cd69887a8c4ba9af13
whut <rafal.klys@wp.pl>
2012-01-08 15:46:55 +0100

    Added empty rails 3.1.3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRails::Application.config.secret_token = '689b75ce98682f6418d4af2b62261522e00b94c7030981541c57436871a7174df448fb5317f4e95acea52e4389553a239b2e488e33c14d16c24d921bc2a142ca'
