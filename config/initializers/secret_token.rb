97be540b756a07e60055239a89cee1efa3719b57
whut <rafalklys@wp.pl>
2012-05-30 17:03:28 +0200

    Added new empty Rails 3.2.3 application for 'depot' application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRails::Application.config.secret_token = '89e292c9fc165e2f60286730bcc35eee32dd05e6e260c5f2948b81a5233395ffe562a32aa4075058c0ef397fc949d9a055bce176b21b9e3228c39224f56d8c4e'
